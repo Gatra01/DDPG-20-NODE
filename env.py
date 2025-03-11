961fc62fbf6d0044967af0aa5374ffe496b10fd7
Gatra01 <87842559+Gatra01@users.noreply.github.com>
2025-03-11 20:48:15 +0700

    Update env.py

--- env.py
+++ env.py
@@ -56,7 +56,7 @@ class GameState:
         total_daya=np.sum(power)
         result_array = np.concatenate((np.array(new_data_rate), np.array([EE]),np.array(power),channel_gain.flatten()))
         fairness = np.var(new_data_rate)  # Variansi untuk mengukur kesenjangan data rate
-        reward = np.sum((np.array(new_data_rate)-self.gamma)*3) -1 * fairness- 3 * np.sum(power[power <= 0])+2*(self.p_max-total_daya)  )
+        reward = np.sum((np.array(new_data_rate)-self.gamma)*3) -1 * fairness- 3 * np.sum(power[power <= 0])+2*(self.p_max-total_daya) 
         #reward =np.sum(((np.array(new_data_rate)-self.gamma)*10).tolist())+ 5*(self.p_max-total_daya) 
         #for i in power :
         #    if i<=0:
