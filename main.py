970577e89a91a95c2401dfc2306d23ffdc3516c4
Gatra01 <87842559+Gatra01@users.noreply.github.com>
2025-03-18 23:06:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -114,11 +114,11 @@ def main():
                 if total_steps % opt.eval_interval == 0:
                     state_eval,inf=eval_env.reset(channel_gain)
                     state_eval = np.array(state_eval, dtype=np.float32)
-                    ep_r,avg_ee = evaluate_policy(channel_gain,state_eval,eval_env, agent, turns=3)
+                    ep_r = evaluate_policy(channel_gain,state_eval,eval_env, agent, turns=3)
                     if opt.write: 
                         writer.add_scalar('ep_r', ep_r, global_step=total_steps)
-                        writer.add_scalar('avg_ee', avg_ee, global_step=total_steps)
-                    print(f'EnvName:{BrifEnvName[opt.EnvIdex]}, Steps: {int(total_steps/1000)}k, Episode Reward:{ep_r}, Energi efisiensi :{avg_ee}')
+                        #writer.add_scalar('avg_ee', avg_ee, global_step=total_steps)
+                    print(f'EnvName:{BrifEnvName[opt.EnvIdex]}, Steps: {int(total_steps/1000)}k, Episode Reward:{ep_r}')
 
                 '''save model'''
                 if total_steps % opt.save_interval == 0:
