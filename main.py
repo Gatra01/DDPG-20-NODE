682ea4aba48d5fdd5f389adab6cf4fbf1358cdb2
Gatra01 <87842559+Gatra01@users.noreply.github.com>
2025-04-06 16:22:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ parser.add_argument('--Loadmodel', type=str2bool, default=False, help='Load pret
 parser.add_argument('--ModelIdex', type=int, default=100, help='which model to load')
 
 parser.add_argument('--seed', type=int, default=0, help='random seed')
-parser.add_argument('--Max_train_steps', type=int, default=100000, help='Max training steps') #aslinya 5e6
+parser.add_argument('--Max_train_steps', type=int, default=200000, help='Max training steps') #aslinya 5e6
 parser.add_argument('--save_interval', type=int, default=2000, help='Model saving interval, in steps.') #aslinya 1e5
 parser.add_argument('--eval_interval', type=int, default=2000, help='Model evaluating interval, in steps.') #aslinya 2e3
 
@@ -37,6 +37,7 @@ def main():
     #EnvName = ['Pendulum-v1','LunarLanderContinuous-v2','Humanoid-v4','HalfCheetah-v4','BipedalWalker-v3','BipedalWalkerHardcore-v3']
     #BrifEnvName = ['PV1', 'LLdV2', 'Humanv4', 'HCv4','BWv3', 'BWHv3']
     BrifEnvName = ['6G', 'LLdV2', 'Humanv4', 'HCv4','BWv3', 'BWHv3']
+    
     # Build Env
     env = GameState(5,3)
     eval_env = GameState(5,3)
@@ -46,6 +47,12 @@ def main():
     #print(f'Env:{EnvName[opt.EnvIdex]}  state_dim:{opt.state_dim}  action_dim:{opt.action_dim}  '
     #      f'max_a:{opt.max_action}  min_a:{env.action_space.low[0]}  max_e_steps:{env._max_episode_steps}')
 
+    #variable tambahan 
+    iterasi = 200
+    total_episode = -(-opt.Max_train_steps//iterasi)
+    sepertiga_eps=total_episode//3
+
+    
     # Seed Everything
     env_seed = opt.seed
     torch.manual_seed(opt.seed)
@@ -81,7 +88,12 @@ def main():
             print('EnvName:', BrifEnvName[opt.EnvIdex], 'score:', score, )
     else:
         total_steps = 0
+        lr_steps = 0
         while total_steps < opt.Max_train_steps: # ini loop episode. Jadi total episode adalah Max_train_steps/200
+            if lr_steps==sepertiga_eps :
+                opt.a_lr=0.5 * opt.a_lr
+                opt.c_lr=0.5 * opt.c_lr
+                lr_steps=0
             loc= env.generate_positions() #lokasi untuk s_t
             channel_gain=env.generate_channel_gain(loc) #channel gain untuk s_t
             s,info= env.reset(channel_gain, seed=env_seed)  # Do not use opt.seed directly, or it can overfit to opt.seed
@@ -91,6 +103,7 @@ def main():
             '''Interact & trian'''
             while not done:  
                 langkah +=1
+                lr_steps+=1
                 if total_steps <= opt.random_steps: #aslinya < aja, ide pengubahan ini tuh supaya selec action di train dulu.
                     a = env.p
                 else: 
@@ -100,7 +113,7 @@ def main():
                 s_next, r, dw, tr, info = env.step(a,channel_gain,next_channel_gain) # dw: dead&win; tr: truncated
                 loc= env.generate_positions()
                 channel_gain=env.generate_channel_gain(loc)
-                if langkah == 150 :
+                if langkah == iterasi :
                     tr= True
                    
                     
