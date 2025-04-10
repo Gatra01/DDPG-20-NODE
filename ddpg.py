bf6a8355879ee7e6d32e11e236460c96d253c551
Gatra01 <87842559+Gatra01@users.noreply.github.com>
2025-04-10 10:04:03 +0700

    Update ddpg.py

--- ddpg.py
+++ ddpg.py
@@ -12,11 +12,11 @@ class DDPG_agent():
 		self.tau = 0.005
 
 		self.actor = Actor(self.state_dim, self.action_dim, self.net_width, self.max_action).to(self.dvc)
-		self.actor_optimizer = torch.optim.RMSprop(self.actor.parameters(), lr=self.a_lr)
+		self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=self.a_lr)
 		self.actor_target = copy.deepcopy(self.actor)
 
 		self.q_critic = Q_Critic(self.state_dim, self.action_dim, self.net_width).to(self.dvc)
-		self.q_critic_optimizer = torch.optim.RMSprop(self.q_critic.parameters(), lr=self.c_lr)
+		self.q_critic_optimizer = torch.optim.Adam(self.q_critic.parameters(), lr=self.c_lr)
 		self.q_critic_target = copy.deepcopy(self.q_critic)
 
 		self.replay_buffer = ReplayBuffer(self.state_dim, self.action_dim, max_size=int(1e5), dvc=self.dvc)
