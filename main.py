28a5c1c8196017b323df06b267027d295944f20f
Gatra01 <87842559+Gatra01@users.noreply.github.com>
2025-05-01 17:57:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -39,8 +39,8 @@ def main():
     BrifEnvName = ['6G', 'LLdV2', 'Humanv4', 'HCv4','BWv3', 'BWHv3']
     
     # Build Env
-    env = GameState(7,3)
-    eval_env = GameState(7,3)
+    env = GameState(7,1)
+    eval_env = GameState(7,1)
     opt.state_dim = env.observation_space
     opt.action_dim = env.action_space
     opt.max_action = env.p_max   #remark: action space【-max,max】
